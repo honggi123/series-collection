83c59579f415854d094b04ce5430f5ec31560f55
honggi123 <89631493+honggi123@users.noreply.github.com>
2023-05-04 21:23:46 +0900

    #15 fetchSereisThumbnail 코루틴 실행 부분 리팩토링 및 supervisorScope 변경

--- app/src/main/java/com/example/series_collector/data/SeriesFetcher.kt
+++ app/src/main/java/com/example/series_collector/data/SeriesFetcher.kt
@@ -1,32 +1,28 @@
 package com.example.series_collector.data
 
 import com.example.series_collector.data.source.YoutubeDataSource
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 import javax.inject.Inject
 
 class SeriesFetcher @Inject constructor(
     private val youtubeDataSource: YoutubeDataSource
 ) {
-    suspend fun fetchSeriesThumbnail(list: List<Series>): List<Series> {
-        val tempList = mutableListOf<Series>()
+
+    suspend fun fetchSeriesThumbnail(list: List<Series>): List<Series> =
         withContext(Dispatchers.IO) {
             list.map { series ->
-                async {
-                    if (series.thumbnail.isNullOrEmpty()) {
-                        series.apply {
-                            val thumbnailUrl = youtubeDataSource.getThumbnailImageUrl(series.seriesId)
-                            tempList.add(copy(thumbnail = thumbnailUrl))
-                        }
-                    } else {
-                        tempList.add(series)
+                supervisorScope {
+                    async {
+                        if (series.thumbnail.isBlank()) {
+                            val thumbnailUrl =
+                                youtubeDataSource.getThumbnailImageUrl(series.seriesId)
+                            series.copy(thumbnail = thumbnailUrl)
+                        } else series
                     }
                 }
             }.awaitAll()
         }
-        return tempList
-    }
+
+
 }
 
