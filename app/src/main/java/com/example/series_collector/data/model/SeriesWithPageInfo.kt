dbab12cf5b6c84d495a5b87087af82e23e312cae
honggi123 <89631493+honggi123@users.noreply.github.com>
2023-06-01 01:46:09 +0900

    refactor: SeriesWithPageInfo와 SeriesEntity의 반복되는 코드로 인한 SeriesWithPageInfo 리팩토링

--- app/src/main/java/com/example/series_collector/data/model/SeriesWithPageInfo.kt
+++ app/src/main/java/com/example/series_collector/data/model/SeriesWithPageInfo.kt
@@ -3,11 +3,6 @@ package com.example.series_collector.data.model
 import com.example.series_collector.data.api.PageInfo
 
 data class SeriesWithPageInfo(
-    val name: String,
-    val description: String,
-    val channel: String,
-    val haveCount: Int,
-    val genre: Int,
-    val thumbnail: String,
+    val series: Series,
     val pageInfo: PageInfo
 )
